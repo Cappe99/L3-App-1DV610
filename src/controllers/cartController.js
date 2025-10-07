32e3e8e05e8078ab8a462fd6fe9d2d1a803f402a
Casper Björlin <cb224cd@student.lnu.se>
2025-10-07 17:00:19 +0200

    Discountcode and shipping cost implemented

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,6 +2,8 @@ import { Cart } from 'l2-module-cart-and-discounts'
 import { products } from '../data/products.js'
 
 const cart = new Cart()
+cart.discountManager.setFreeShippingThreshold(1000)
+cart.discountManager.shippingCost = 49
 
 /**
  *
@@ -19,7 +21,9 @@ export class CartController {
       total: cart.getTotalPriceafterDiscounts(),
       shipping: cart.getShippingCost(),
       final: cart.getFinalPrice(),
-      quantity: cart.getTotalQuantityInCart()
+      quantity: cart.getTotalQuantityInCart(),
+      discountSuccess: false,
+      discountCode: null
     })
   }
 
@@ -67,9 +71,28 @@ export class CartController {
       return res.status(404).send('Produkt hittades inte')
     }
 
-    // Tar bort EN produkt i taget (inte hela raden)
     cart.removeProductFromCart(product, 1)
 
     res.redirect('/cart')
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
+  applyDiscount (req, res) {
+    const code = req.body.code
+    const success = cart.discountManager.applyDiscountCode(code)
+
+    res.render('cart/index', {
+      items: cart.items,
+      total: cart.getTotalPriceafterDiscounts(),
+      shipping: cart.getShippingCost(),
+      final: cart.getFinalPrice(),
+      quantity: cart.getTotalQuantityInCart(),
+      discountSuccess: success,
+      discountCode: code
+    })
   }
 }
