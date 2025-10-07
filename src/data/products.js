fb642678e87cca30476430153ebe5712e13c4eb9
Casper Björlin <cb224cd@student.lnu.se>
2025-10-07 15:05:19 +0200

    Products and css

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,11 @@
 export const products = [
   { id: 1, name: 'T-shirt', price: 299, image: '/images/t-shirt.webp', description: 'Tröja' },
-  { id: 2, name: 'T-shirt', price: 900, image: '/images/t-shirt.webp' }
+  { id: 3, name: 'Cap', price: 300, image: '/images/cap.png' },
+  { id: 4, name: 'Hoddie', price: 700, image: '/images/hoodie2.png' },
+  { id: 5, name: 'Sneakers', price: 1200, image: '/images/sneakers.png' },
+  { id: 6, name: 'Hat', price: 300, image: '/images/hat.png' },
+  { id: 7, name: 'Jacket', price: 3000, image: '/images/jacka.png' },
+  { id: 8, name: 'Sunglasses', price: 800, image: '/images/sunglasses.png' },
+  { id: 9, name: 'Pants', price: 900, image: '/images/byxor.png' }
+
 ]
