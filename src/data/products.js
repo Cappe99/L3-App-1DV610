c82d3b67094b4c86eb4c58c90c0a39e9d4b6c896
Casper Björlin <cb224cd@student.lnu.se>
2025-10-07 12:22:27 +0200

    CSS and grid

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,4 @@
 export const products = [
-  { id: 1, name: 'T-shirt', price: 299, image: '/images/t-shirt.webp' },
+  { id: 1, name: 'T-shirt', price: 299, image: '/images/t-shirt.webp', description: 'Tröja' },
   { id: 2, name: 'T-shirt', price: 900, image: '/images/t-shirt.webp' }
 ]
