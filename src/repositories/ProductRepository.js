73e9f454c63f8e4e91ca698e8f7251e219a6c921
Casper Björlin <cb224cd@student.lnu.se>
2025-10-21 14:35:43 +0200

    Remoove jsDoc

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -1,20 +1,11 @@
 import { products } from '../data/products.js'
 
-/**
- *
- */
 export class ProductRepository {
-  /**
-   *
-   * @param id
-   */
+
   findById (id) {
     return products.find(p => p.id === id)
   }
 
-  /**
-   *
-   */
   findAll () {
     return products
   }
